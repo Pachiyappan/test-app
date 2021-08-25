350bedab51afb66b95daf61c04a582e22403e383
Pachiyappan <pachi@fidisys.com>
2021-08-25 08:57:31 +0530

    Scss package added

---
+++ src/Common.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./Common.module.scss";
+
+const Common = props => {
+    return (
+        <div className={styles.common}>
+            Temp Component
+
+        </div>
+    );
+};
+
+Common.propTypes = {
+
+};
+
+export default Common;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Common from './Common';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Common />
   </React.StrictMode>,
   document.getElementById('root')
 );
